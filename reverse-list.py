028645163e6efb82df32a51e71350dc214444aab
Habibou Moussa <habibou997@gmail.com>
2023-04-07 10:59:33 +0000

    reverse a list using negative slicing

--- reverse-list.py
+++ reverse-list.py
@@ -1,4 +1,9 @@
-#Using the function reverse()
-list = [100,200,300,400,1,900]
-list.reverse()
-print(list)
+#solution : Using the function reverse()
+list1 = [100,200,300,400,1,900]
+list1.reverse()
+print(list1)
+
+#Solution2 : Using negative slicing
+users = ['habibou','moussa','daouda','karima','mahamadou']
+users = users[::-1]
+print(users)
